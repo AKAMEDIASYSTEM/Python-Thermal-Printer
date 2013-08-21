bb4a0f4cfc4aa0245c9bb72178c23612c0e45e67
Noah Feehan <nfeehan@gmail.com>
2013-08-21 15:13:19 -0400

    test script

--- hotpants.py
+++ hotpants.py
@@ -7,8 +7,28 @@ import Adafruit_BBIO.ADC as adc
 import Adafruit_BBIO.GPIO as gpio
 import time
 from serial import Serial
+import atexit
+
+sensor_pin = 'P9_40'
+
 
 printer = Adafruit_Thermal("/dev/ttyO2", 19200, timeout=5)
 printer.begin()
-printer.feed()
+printer.feed(3)
 printer.print("o hai")
+
+def checkSensor():
+	r = adc.read(sensor_pin)
+	printer.print(r)
+	printer.feed(3)
+
+def exit_handler():
+    print 'exiting'
+    adc.cleanup()
+    # uart.cleanup() # not yet supported?
+
+if __name__ == '__main__':
+	atexit.register(exit_handler)
+	while True:
+		checkSensor()
+		time.sleep(30)
