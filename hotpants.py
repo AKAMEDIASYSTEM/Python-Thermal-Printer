faf78288a5596484e74e30ff70780926b9a3b38b
Noah Feehan <nfeehan@gmail.com>
2013-08-21 14:54:07 -0400

    import mistake

--- hotpants.py
+++ hotpants.py
@@ -1,12 +1,11 @@
 # oh boy oh boy!
 # It's project HOT_PANTS
-
+from __future__ import print_function
 import Adafruit_BBIO.UART as uart
 from Adafruit_Thermal import *
 import Adafruit_BBIO.ADC as adc
 import Adafruit_BBIO.GPIO as gpio
 import time
-from __future__ import print_function
 from serial import Serial
 
 printer = Adafruit_Thermal("/dev/ttyO2", 19200, timeout=5)
