8df604f25ea0b57dce27233c08cdcdecbb0e5e83
Noah Feehan <nfeehan@gmail.com>
2013-08-26 14:54:06 -0400

    more sensor-testing

--- vcnl4000.py
+++ vcnl4000.py
@@ -35,7 +35,7 @@ class VCNL4000():
   VCNL4000_AMBIENTREADY = 0x40
   VCNL4000_PROXIMITYREADY = 0x20
 
-  i2c
+  i2c = None
 
   def __init__(self, *args, **kwargs):
     i2c = Adafruit_I2C(self.VCNL4000_ADDRESS)
