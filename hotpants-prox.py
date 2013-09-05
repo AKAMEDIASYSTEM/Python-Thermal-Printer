8018c00ea63846334b4f379f553810a2356f67a0
Noah Feehan <nfeehan@gmail.com>
2013-09-05 14:14:32 -0400

    added systemd timer

--- hotpants-prox.py
+++ hotpants-prox.py
@@ -11,6 +11,10 @@ import random
 import atexit
 import VCNL4000 as vcnl
 
+
+uart.setup("UART2")
+atexit.register(exit_handler)
+
 sensor_pin = 'P9_40'
 extreme_lo = ['dark','inky','shadowed','midnight''black','sinister','dour','glowering','glum','moody','morose','saturnine','sour','sullen','benighted','obscure','blue','dingy','disconsolate','dismal','gloomy','grim','sorry','drab','drear','dreary','colored','coloured','dark-skinned','non-white','depressing','dispiriting']
 mid_lo = ['shady','dim','grey','faint','weak','dim','shadowy','vague','wispy','feeble','light','swooning','light-headed','lightheaded','fainthearted','timid','faint-hearted','cloudy','muddy','murky','turbid']
@@ -33,37 +37,37 @@ rPast = 0
 emission_threshold = 100
 
 def parseLen(text):
-	L = []
-	# add newlines to cause text to print properly
-	# we need this because we're printing upside-down text
-	# "call the police, it is faint-hearted" should be printed as
-	# "rted" then linebreak then "Call the police, it is faint-hea"
-	# which is "rted\nCall the police, it is faint-hea"
+    L = []
+    # add newlines to cause text to print properly
+    # we need this because we're printing upside-down text
+    # "call the police, it is faint-hearted" should be printed as
+    # "rted" then linebreak then "Call the police, it is faint-hea"
+    # which is "rted\nCall the police, it is faint-hea"
 
-	if len(text) > Adafruit_Thermal.maxColumn: # 32 is defined by the printer; max chars per line
-		r = len(text)%32
-		L.append(text[-r:]+'\n')
-		for i in reversed(range(len(text)/32)):
-			L.append(text[i*32:(i+1)*32]+'\n')
-	else:
-		L.append(text)
-	return ''.join(L)
+    if len(text) > Adafruit_Thermal.maxColumn: # 32 is defined by the printer; max chars per line
+        r = len(text)%32
+        L.append(text[-r:]+'\n')
+        for i in reversed(range(len(text)/32)):
+            L.append(text[i*32:(i+1)*32]+'\n')
+    else:
+        L.append(text)
+    return ''.join(L)
 
 def checkSensor():
-	global rPast
-	# r = adc.read(sensor_pin)
-	r = v.readProximity()
-	delta = r-rPast
-	if abs(delta) > emission_threshold:
-		if delta < 0:
-			printer.print(parseLen(random.choice(preamble) + random.choice(extreme_hi)))
-			printer.feed(1)
-		else:
-			printer.print(parseLen(random.choice(preamble) + random.choice(extreme_lo)))
-			printer.feed(1)
-		printer.print(str(r)+', delta: '+str(delta)) # debug - prints sensor val that elicited the text
-		printer.feed(2)
-	rPast = r
+    global rPast
+    # r = adc.read(sensor_pin)
+    r = v.readProximity()
+    delta = r-rPast
+    if abs(delta) > emission_threshold:
+        if delta < 0:
+            printer.print(parseLen(random.choice(preamble) + random.choice(extreme_hi)))
+            printer.feed(1)
+        else:
+            printer.print(parseLen(random.choice(preamble) + random.choice(extreme_lo)))
+            printer.feed(1)
+        printer.print(str(r)+', delta: '+str(delta)) # debug - prints sensor val that elicited the text
+        printer.feed(2)
+    rPast = r
 
 def exit_handler():
     pass
@@ -71,10 +75,15 @@ def exit_handler():
     # adc.cleanup()
     # uart.cleanup() # not yet supported?
 
-if __name__ == '__main__':
-	# adc.setup()
-	uart.setup("UART2")
-	atexit.register(exit_handler)
-	while True:
-		checkSensor()
-		time.sleep(0.5)
+while True:
+    checkSensor()
+    time.sleep(0.5)
+
+
+# if __name__ == '__main__':
+#     # adc.setup()
+#     uart.setup("UART2")
+#     atexit.register(exit_handler)
+#     while True:
+#         checkSensor()
+#         time.sleep(0.5)
